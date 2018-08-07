e461ac9280a60bba9e2e3a82162b0828e55cd709
richjimdev <richjimdev@gmail.com>
2018-08-07 14:45:26 -0700

    Made time head row work

--- js/app.js
+++ js/app.js
@@ -9,13 +9,17 @@ function SalmonCookies(loc, minCust, maxCust, avrg) {
   this.avrg = avrg;
 }
 
+//All location objects being created by the constructor
 var $1np = new SalmonCookies('1st and Pike', 23, 65, 6.3);
 var $sta = new SalmonCookies('SeaTac Airport', 3, 24, 1.2);
 var $seaC = new SalmonCookies('Seattle Center', 11, 38, 1.2);
 var $capHill = new SalmonCookies('Capitol Hill', 20, 38, 3.7);
 var $alki = new SalmonCookies('Alki', 2, 16, 4.6);
+
+//giving the compile function to all location objects
 SalmonCookies.prototype.showData = compile;
 
+//function to calculate the random customers
 function getAvrgCookies(location) {
   var randNum = Math.floor(Math.random() * (location.maxCust - location.minCust + 1) + location.minCust);
   // console.log(`the random number generated for ${$1np.loc} was ${randNum}`);
@@ -23,6 +27,7 @@ function getAvrgCookies(location) {
   return Math.round(total);
 }
 
+//function to display time in 12hour format
 function makeTime(hour) {
   var date = new Date(`August 06, 2018 ${hour}:00:00`);
   var dateOptions = {
@@ -30,6 +35,15 @@ function makeTime(hour) {
     hour12: true
   };
   return date.toLocaleString('en-US', dateOptions);
+}
+
+var timesOnTable;
+var timeOnTableRow = document.getElementById('times-row');
+
+for (var i = 6; i <= 20; i++) {
+  timesOnTable = document.createElement('th');
+  timesOnTable.textContent = makeTime(i);
+  timeOnTableRow.appendChild(timesOnTable);
 }
 
 var sum = 0;
